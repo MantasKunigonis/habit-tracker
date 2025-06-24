c1b4ef4583934cf43a3a74829d079ac01729ae63
Mantas <mantaskunigonis@gmail.com>
2025-06-24 00:04:44 -0400

    Initialize Flask project and fix black, flake8, and husky

---
+++ backend/app/__init__.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from config import Config
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+    from .routes import main
+    app.register_blueprint(main)
+    return app
