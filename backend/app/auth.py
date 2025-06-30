59ee860a097b5238688f4da49915d63515cb94dd
Mantas <mantaskunigonis@gmail.com>
2025-06-29 23:51:09 -0400

    Resolve circulary import issue by moving things to extensions.py

--- backend/app/auth.py
+++ backend/app/auth.py
@@ -1,8 +1,8 @@
 from flask import Blueprint, render_template, request, redirect, url_for, flash
 from flask_login import login_user, logout_user, login_required
 from .models import User
-from . import db
-from werkzaug.security import generate_password_hash, check_password_hash
+from .extensions import db
+from werkzeug.security import generate_password_hash, check_password_hash
 
 auth = Blueprint('auth', __name__)
 
